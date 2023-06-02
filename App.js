01191a45962e5b15a0984f431c8827609b238850
cristibadea777 <cristibadea777@gmail.com>
2023-06-02 11:12:39 +0300

    Initializare

--- App.js
+++ App.js
@@ -4,17 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={ [ styles.text, {} ]}>IC XC NI KA !!!</Text>
     </View>
   );
 }
 
+//culori  #232B2B #1e1e1e
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#232B2B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 33,
+    color: 'white'
+  },
+
+
 });
